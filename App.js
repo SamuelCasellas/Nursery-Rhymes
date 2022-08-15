2ef6ef4a1a98c06fe8d2122e3ea43d7d93258337
Samuel Casellas <sam.casellas@gmail.com>
2022-08-15 15:32:27 -0600

    configure App.js navigation

--- App.js
+++ App.js
@@ -3,26 +3,42 @@ import { createAppContainer } from "react-navigation";
 
 import { createSwitchNavigator } from "react-navigation";
 // import currently not working
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "react-navigation-stack";
 
 import { Provider as MusicProvider } from "./src/context/MusicContext";
 import { Provider as SettingsProvider } from "./src/context/SettingsContext";
 
 import TitleScreen from "./src/screens/TitleScreen";
+import ShowTracksScreen from "./src/screens/ShowTracksScreen";
 import PlayMusicScreen from "./src/screens/PlayMusicScreen";
+import SMCScreen from "./src/screens/SMCScreen";
+import SettingsScreen from "./src/screens/SettingsScreen";
 
-const navigator = createStackNavigator({
-  Title: TitleScreen,
-  Music: PlayMusicScreen
-}, {
-  initialRouteName: "Music",
-  navigationOptions: {
-    title: "APP_NAME"
-  }
+const musicFlow = createStackNavigator({
+  ShowTracks: ShowTracksScreen,
+  PlayMusic: PlayMusicScreen
 });
 
-const App = createAppContainer(navigator);
+const mainMenuTabFlow = createBottomTabNavigator({
+  musicFlow,
+  SMC: SMCScreen,
+  Settings: SettingsScreen
+});
+
+const switchNavigator = createSwitchNavigator({
+  Title: TitleScreen,
+  mainMenuTabFlow
+}, 
+// {
+//   initialRouteName: "Title",
+//   navigationOptions: {
+//     title: "APP_NAME"
+//   }
+// }
+);
+
+const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
