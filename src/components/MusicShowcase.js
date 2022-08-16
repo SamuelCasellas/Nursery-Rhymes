5ea488d15746577e3b7faf508e8cbdc20ce0b945
Samuel Casellas <sam.casellas@gmail.com>
2022-08-16 09:07:30 -0600

    Added slider for music

--- src/components/MusicShowcase.js
+++ src/components/MusicShowcase.js
@@ -1,21 +1,49 @@
-import React, { useContext } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { View, StyleSheet, FlatList } from "react-native";
 import { Text } from "react-native-elements";
 import { Context as MusicContext } from "../context/MusicContext";
 
-// import Slider from "@react-native-community/slider";
+import Slider from "@react-native-community/slider";
+import classicalSongs from "../models/classicalSongs";
 
 const MusicShowcase = () => {
   const { state } = useContext(MusicContext);
 
+  let color = "#FFF";
+
+  // const renderSongs = ({ index, item }) => {
+  //   return (
+  //     <View style={{}}>
+
+  //     </View>
+  //   )
+  // };
+
   return (
     <>
       <View>
-        <Text style={styles.songTitle}>Currently playing: {state.currentTrack}</Text>
+        {/* <Text style={styles.songTitle}>Currently playing: {state.currentTrack}</Text> */}
+        {/* <FlatList
+          data={classicalSongs}
+        /> */}
       </View>
-      <View>
-        {/* <Slider /> */}
+      <View> 
+        <Slider 
+          style={styles.slider}
+          value={10}
+          minimumValue={0}
+          maximumValue={100}
+          thumbTintColor="#FFD369"
+          minimumTrackTintColor="#FFD369"
+          maximumTrackTintColor="#FFF"
+          onSlidingComplete={()=>{}}
+        />
+        <View style={styles.trackProgress}>
+          <Text style={{ color }}>0:00</Text>
+          <Text style={{ color }}>3:50</Text>
+        </View>
       </View>
+
     </>
   );
 };
@@ -24,14 +52,21 @@ const styles = StyleSheet.create({
   songTitle: {
     fontSize: 18,
     fontWeight: "600",
-    textAlign: "center"
+    textAlign: "center",
+    color: "#EEEEEE"
   },
   slider: {
     width: 350,
-    height: 400,
+    height: 40,
     marginTop: 25,
     flexDirection: "row"
-  }
+  },
+  trackProgress: {
+    width: 340,
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
 });
 
 export default MusicShowcase;
